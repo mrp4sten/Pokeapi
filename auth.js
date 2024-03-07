7e64a44d97a39f3fca0578dbdf6c4dd0e1a2b4c1
mrp4sten <mauricio.pasten.martinez@gmail.com>
2024-03-06 19:19:51 -0600

    Adding auth
    Adding jsonwebtoken, passport and passport-jwt to work with Auth.

---
+++ auth.js
@@ -0,0 +1,17 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+const secret = 'your_jwt_secret'; // Todo: Change this to an environment variable
+
+const auth = (passport) => {
+  const options = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('jwt'),
+    secretOrKey: `${secret}`
+  }
+
+  passport.use(new Strategy(options, (decoded, done) => {
+    console.log('decoded jwt', decoded)
+    return done(null, decoded)
+  }))
+}
+
+export default auth
--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 import express from "express"
+import passport from "passport"
+import auth from "./auth.js"
+
 const app = express()
+auth(passport)
+
+app.post('/login', (req, res) => {
+  res.status(200).json({token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.xPBQIypGa9i-DZCCx4hqbSaWcFEd0sFuWZ92N516qg0'})
+})
 
 app.post('/team/pokemons', (req, res) => {
   res.send('Hello World')
 })
 
-app.get('/team', (req, res) => {
-  res.send('Hello World')
+app.get('/team', passport.authenticate('jwt', {session: false}), (req, res) => {
+  res.status(200).send('Hello World')
 })
 
 app.delete('/team/pokemons/:id', (req, res) => {
